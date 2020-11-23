17d8bfb6bbe60d6a37d26dc422a36fe826c6d1d0
Могилин Виктор <v.mogilin@corp.mail.ru>
2020-11-24 00:49:40 +0300

    Rocksdb is open and store values

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+std::string kDBPath = "/tmp/rocksdb_simple_example";
+namespace rocks = ROCKSDB_NAMESPACE;
 
 int main(int argc, char *argv[]) {
-    std::cerr << "hi" << std::endl;
+    rocks::DB* db;
+    rocks::Options options;
+
+    // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+    options.IncreaseParallelism();
+    options.OptimizeLevelStyleCompaction();
+
+    // create the DB if it's not already present
+    options.create_if_missing = true;
+
+    // open DB
+    rocks::Status s = rocks::DB::Open(options, kDBPath, &db);
+    assert(s.ok());
+
+    s = db->Put(rocks::WriteOptions(), "key1", "value");
+    assert(s.ok());
+    std::string value;
+
+    s = db->Get(rocks::ReadOptions(), "key1", &value);
+
+    assert(s.ok());
+    assert(value == "value");
+
+    delete db;
     return 0;
 }
